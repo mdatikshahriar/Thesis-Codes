3aaa73bee4a27165f3ea29aab4b59d61929f7a86
mdatikshahriar <md.atikshahriar728@gmail.com>
2021-07-08 00:08:30 +0600

    updated

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 mkdir .temp
 attrib +h .temp
 cp -r fabric-samples/goods-ledger .temp
-cp -r fabric-samples/4host-swarm .temp
+cp -r fabric-samples/4host-swarm-default .temp
+cp -r fabric-samples/4host-swarm-custom .temp
 cp fabric-samples/run.sh .temp
 cp fabric-samples/stop.sh .temp
 cp fabric-samples/.gitignore .temp
@@ -10,7 +11,8 @@ rm -rf fabric-samples
 curl https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s 2.2.0
 
 cp -r .temp/goods-ledger fabric-samples
-cp -r .temp/4host-swarm fabric-samples
+cp -r .temp/4host-swarm-default fabric-samples
+cp -r .temp/4host-swarm-custom fabric-samples
 cp .temp/run.sh fabric-samples
 cp .temp/stop.sh fabric-samples
 cp .temp/.gitignore fabric-samples
--- uninstall.sh
+++ uninstall.sh
@@ -7,7 +7,8 @@ cd ..
 mkdir .temp
 attrib +h .temp
 cp -r fabric-samples/goods-ledger .temp
-cp -r fabric-samples/4host-swarm .temp
+cp -r fabric-samples/4host-swarm-default .temp
+cp -r fabric-samples/4host-swarm-custom .temp
 cp fabric-samples/run.sh .temp
 cp fabric-samples/stop.sh .temp
 cp fabric-samples/.gitignore .temp
@@ -16,7 +17,8 @@ rm -rf fabric-samples
 mkdir fabric-samples
 
 cp -r .temp/goods-ledger fabric-samples
-cp -r .temp/4host-swarm fabric-samples
+cp -r .temp/4host-swarm-default fabric-samples
+cp -r .temp/4host-swarm-custom fabric-samples
 cp .temp/run.sh fabric-samples
 cp .temp/stop.sh fabric-samples
 cp .temp/.gitignore fabric-samples
